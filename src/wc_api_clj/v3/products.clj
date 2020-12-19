96fc1d9ec627b122026e4ff69ca5630eb3b9f5e9
Khan Mohammad R <codemascot@hotmail.com>
2020-12-19 17:37:07 +0600

    Documentation strings fix for products.clj

--- src/wc_api_clj/v3/products.clj
+++ src/wc_api_clj/v3/products.clj
@@ -6,7 +6,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn create-product
-  "Create product(s) and it's needed basic authentication."
+  "Create product(s)."
   [{:keys [url consumer_key consumer_secret body exception insecure]}]
   (try (woo/post-req {:url (str url "/wp-json/wc/v3/products")
                       :username consumer_key
@@ -17,29 +17,29 @@
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn get-product-by-id
-  "Retrieve product by ID and it need basic authentication."
+  "Retrieve product by the product's ID."
   [{:keys [url consumer_key consumer_secret product exception insecure]}]
-  (try (woo/get-req-auth {:siteurl url
-                          :uri (str "/wp-json/wc/v3/products/" product)
-                          :username consumer_key
-                          :password consumer_secret
-                          :exception (not (not exception))
-                          :insecure (not (not insecure))})
+  (try (woo/get-req {:siteurl url
+                     :uri (str "/wp-json/wc/v3/products/" product)
+                     :username consumer_key
+                     :password consumer_secret
+                     :exception (not (not exception))
+                     :insecure (not (not insecure))})
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn get-products
-  "Retrieve all products and it need basic authentication."
+  "Retrieve all products."
   [{:keys [url consumer_key consumer_secret exception insecure]}]
-  (try (woo/get-req-auth {:siteurl url
-                          :uri "/wp-json/wc/v3/products/"
-                          :username consumer_key
-                          :password consumer_secret
-                          :exception (not (not exception))
-                          :insecure (not (not insecure))})
+  (try (woo/get-req {:siteurl url
+                     :uri "/wp-json/wc/v3/products/"
+                     :username consumer_key
+                     :password consumer_secret
+                     :exception (not (not exception))
+                     :insecure (not (not insecure))})
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn update-product-by-id
-  "Create product(s) and it's needed basic authentication."
+  "Update a product by the product's ID."
   [{:keys [url consumer_key consumer_secret product body exception insecure]}]
   (try (woo/post-req {:url (str url "/wp-json/wc/v3/products/" product)
                       :username consumer_key
@@ -50,7 +50,7 @@
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn delete-product-by-id
-  "Retrieve product by ID and it need basic authentication."
+  "Delete a product by the porduct's ID."
   [{:keys [url consumer_key consumer_secret product exception insecure]}]
   (try (woo/delete-req {:siteurl url
                         :uri (str "/wp-json/wc/v3/products/" product "?force=true")
@@ -61,7 +61,7 @@
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn products-batch-update
-  "Batch update product(s) and it need basic authentication."
+  "CRUD multiple products in a batch."
   [{:keys [url consumer_key consumer_secret body exception insecure]}]
   (try (woo/post-req {:url (str url "/wp-json/wc/v3/products/batch")
                       :username consumer_key
