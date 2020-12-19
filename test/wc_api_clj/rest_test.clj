328108433825da93bc293158d13cce2b1ce03fe1
Khan Mohammad R <codemascot@hotmail.com>
2020-12-19 17:36:06 +0600

    Barebone test file for rest.clj

---
+++ test/wc_api_clj/rest_test.clj
@@ -0,0 +1,7 @@
+(ns wc-api-clj.rest-test
+  (:require [clojure.test :refer :all]
+            [wc-api-clj.rest :refer :all]))
+
+;;;;;;;;;;;;;;;;;;;;;;
+;; Testing rest.clj ;;
+;;;;;;;;;;;;;;;;;;;;;;
