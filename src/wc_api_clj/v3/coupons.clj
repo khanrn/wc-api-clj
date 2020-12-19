868718afd542199ae58e5076f130da45602c8ccc
Khan Mohammad R <codemascot@hotmail.com>
2020-12-19 17:38:11 +0600

    Documentation strings fix for coupons.clj

--- src/wc_api_clj/v3/coupons.clj
+++ src/wc_api_clj/v3/coupons.clj
@@ -6,7 +6,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn create-coupon
-  "Create coupon(s) and it need basic authentication."
+  "Create coupon(s)."
   [{:keys [url consumer_key consumer_secret body exception insecure]}]
   (try (woo/post-req {:url (str url "/wp-json/wc/v3/coupons")
                       :username consumer_key
@@ -17,29 +17,29 @@
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn get-coupon-by-id
-  "Retrieve coupon by ID and it need basic authentication."
+  "Retrieve a coupon by the coupon's ID."
   [{:keys [url consumer_key consumer_secret coupon exception insecure]}]
-  (try (woo/get-req-auth {:siteurl url
-                          :uri (str "/wp-json/wc/v3/coupons/" coupon)
-                          :username consumer_key
-                          :password consumer_secret
-                          :exception (not (not exception))
-                          :insecure (not (not insecure))})
+  (try (woo/get-req {:siteurl url
+                     :uri (str "/wp-json/wc/v3/coupons/" coupon)
+                     :username consumer_key
+                     :password consumer_secret
+                     :exception (not (not exception))
+                     :insecure (not (not insecure))})
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn get-coupons
-  "Retrieve all coupons and it need basic authentication."
+  "Retrieve all coupons."
   [{:keys [url consumer_key consumer_secret exception insecure]}]
-  (try (woo/get-req-auth {:siteurl url
-                          :uri "/wp-json/wc/v3/coupons/"
-                          :username consumer_key
-                          :password consumer_secret
-                          :exception (not (not exception))
-                          :insecure (not (not insecure))})
+  (try (woo/get-req {:siteurl url
+                     :uri "/wp-json/wc/v3/coupons/"
+                     :username consumer_key
+                     :password consumer_secret
+                     :exception (not (not exception))
+                     :insecure (not (not insecure))})
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn update-coupon-by-id
-  "Create coupon(s) and it's needed basic authentication."
+  "Update a coupon by the coupon's ID."
   [{:keys [url consumer_key consumer_secret coupon body exception insecure]}]
   (try (woo/post-req {:url (str url "/wp-json/wc/v3/coupons/" coupon)
                       :username consumer_key
@@ -50,7 +50,7 @@
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn delete-coupon-by-id
-  "Retrieve coupon by ID and it need basic authentication."
+  "Delete a coupon by the coupon's ID."
   [{:keys [url consumer_key consumer_secret coupon exception insecure]}]
   (try (woo/delete-req {:siteurl url
                         :uri (str "/wp-json/wc/v3/coupons/" coupon "?force=true")
@@ -61,7 +61,7 @@
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn coupons-batch-update
-  "Batch update coupon(s) and it need basic authentication."
+  "CRUD multiple coupons in a batch."
   [{:keys [url consumer_key consumer_secret body exception insecure]}]
   (try (woo/post-req {:url (str url "/wp-json/wc/v3/coupons/batch")
                       :username consumer_key
