fa596c9ebe8635982fd2b3c8669dcf0992cc0539
Khan Mohammad R <codemascot@hotmail.com>
2020-12-19 17:37:57 +0600

    Documentation strings fix for orders.clj

--- src/wc_api_clj/v3/orders.clj
+++ src/wc_api_clj/v3/orders.clj
@@ -6,7 +6,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn create-order
-  "Create order(s) and it's needed basic authentication."
+  "Create order(s)."
   [{:keys [url consumer_key consumer_secret body exception insecure]}]
   (try (woo/post-req {:url (str url "/wp-json/wc/v3/orders")
                       :username consumer_key
@@ -17,29 +17,29 @@
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn get-order-by-id
-  "Retrieve order by ID and it need basic authentication."
+  "Retrieve an order by the order's ID."
   [{:keys [url consumer_key consumer_secret order exception insecure]}]
-  (try (woo/get-req-auth {:siteurl url
-                          :uri (str "/wp-json/wc/v3/orders/" order)
-                          :username consumer_key
-                          :password consumer_secret
-                          :exception (not (not exception))
-                          :insecure (not (not insecure))})
+  (try (woo/get-req {:siteurl url
+                     :uri (str "/wp-json/wc/v3/orders/" order)
+                     :username consumer_key
+                     :password consumer_secret
+                     :exception (not (not exception))
+                     :insecure (not (not insecure))})
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn get-orders
-  "Retrieve all orders and it need basic authentication."
+  "Retrieve all orders."
   [{:keys [url consumer_key consumer_secret exception insecure]}]
-  (try (woo/get-req-auth {:siteurl url
-                          :uri "/wp-json/wc/v3/orders/"
-                          :username consumer_key
-                          :password consumer_secret
-                          :exception (not (not exception))
-                          :insecure (not (not insecure))})
+  (try (woo/get-req {:siteurl url
+                     :uri "/wp-json/wc/v3/orders/"
+                     :username consumer_key
+                     :password consumer_secret
+                     :exception (not (not exception))
+                     :insecure (not (not insecure))})
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn update-order-by-id
-  "Create order(s) and it's needed basic authentication."
+  "Update an order by the order's ID."
   [{:keys [url consumer_key consumer_secret order body exception insecure]}]
   (try (woo/post-req {:url (str url "/wp-json/wc/v3/orders/" order)
                       :username consumer_key
@@ -50,7 +50,7 @@
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn delete-order-by-id
-  "Retrieve order by ID and it need basic authentication."
+  "Delete an order by the order's ID."
   [{:keys [url consumer_key consumer_secret order exception insecure]}]
   (try (woo/delete-req {:siteurl url
                         :uri (str "/wp-json/wc/v3/orders/" order "?force=true")
@@ -61,7 +61,7 @@
        (catch clojure.lang.ExceptionInfo e (str (.getMessage e)))))
 
 (defn orders-batch-update
-  "Batch update order(s) and it need basic authentication."
+  "CRUD multiple products in a batch."
   [{:keys [url consumer_key consumer_secret body exception insecure]}]
   (try (woo/post-req {:url (str url "/wp-json/wc/v3/orders/batch")
                       :username consumer_key
