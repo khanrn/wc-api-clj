2f4107fc74b3110a50c133952f1523ad75af9cdf
Khan Mohammad R <codemascot@hotmail.com>
2020-12-24 11:07:13 +0600

    FIX #10, test for EDN to query string function added

--- test/wc_api_clj/util_test.clj
+++ test/wc_api_clj/util_test.clj
@@ -25,3 +25,8 @@
     (is (= false (util/is-url "http://www.example.com//")))
     (is (= false (util/is-url "http://example.com///")))
     (is (= false (util/is-url "http://example.com//")))))
+
+(deftest edn-to-query-str-utility-function-test
+  (testing "Testing `edn-to-query-str` utility functions."
+    (is (= "?force=true&id=34&post_type=page"
+           (util/edn-to-query-str {:force true :id 34 :post_type "page"})))))
